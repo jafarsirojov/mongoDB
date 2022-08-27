1a8b340d041ca6cbdc337336a554b4fb611d2976
jafarsirojov <sysadjafarpr@gmail.com>
2022-08-27 20:42:38 +0500

    feat: cache get duration from config

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	go.mongodb.org/mongo-driver v1.10.1
 	go.uber.org/fx v1.18.1
 	go.uber.org/zap v1.16.0
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,13 +5,17 @@ import (
 	"fmt"
 	"go.uber.org/fx"
 	"io"
+	"log"
 	"os"
+	"strconv"
+	"time"
 )
 
 type Config struct {
-	MongoDbUrl string
-	Port       string
-	Version    string
+	MongoDbUrl                string
+	Port                      string
+	Version                   string
+	ResetRecordsCacheDuration time.Duration
 }
 
 var Module = fx.Options(fx.Provide(ProvideConfig))
@@ -19,6 +23,17 @@ var Module = fx.Options(fx.Provide(ProvideConfig))
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
+	}
+	return fallback
+}
+
+func getEnvDuration(key string, fallback time.Duration) time.Duration {
+	if s, ok := os.LookupEnv(key); ok {
+		v, err := strconv.Atoi(s)
+		if err != nil {
+			log.Fatalf("bad value %q for %s: %v", s, key, err)
+		}
+		return time.Duration(v)
 	}
 	return fallback
 }
@@ -32,8 +47,9 @@ func (c *Config) Dump(w io.Writer) error {
 
 func ProvideConfig() *Config {
 	return &Config{
-		MongoDbUrl: getEnv("MONGO_DB_URL", "mongodb://localhost:27017/"),
-		Port:       getEnv("PORT", ":7777"),
-		Version:    getEnv("VERSION", "1"),
+		MongoDbUrl:                getEnv("MONGO_DB_URL", "mongodb://localhost:27017/"),
+		Port:                      getEnv("PORT", ":7777"),
+		Version:                   getEnv("VERSION", "1"),
+		ResetRecordsCacheDuration: getEnvDuration("RESET_RECORDS_CACHE_DURATION", time.Second),
 	}
 }
